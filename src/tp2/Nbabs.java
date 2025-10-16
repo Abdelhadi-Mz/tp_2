bb07b0cf295d9c5c084673b579c96caa9c6f0afc
Abdelhadi-Mz <elmezouariabdelhadi@gmail.com>
2025-10-17 00:39:10 +0100

    Update Nbabs.java

--- src/tp2/Nbabs.java
+++ src/tp2/Nbabs.java
@@ -1,5 +1,27 @@
 package tp2;
 
-public class Nbabs {
+import java.util.Scanner;
 
+public class Nbabs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt();
+
+        boolean[] present = new boolean[n + 1];
+        for (int num : arr) {
+            if (num >= 1 && num <= n) present[num] = true;
+        }
+
+        System.out.print("Missing elements: ");
+        for (int i = 1; i <= n; i++) {
+            if (!present[i]) System.out.print(i + " ");
+        }
+
+        scanner.close();
+    }
 }
+
